017be978d0bdb715f43b83c8e583011669bcee48
Muhammad Syaiful Rahman <99336708+ifulrahman@users.noreply.github.com>
2025-09-29 12:58:23 +0700

    Update import at routeList (typo)

--- src/routes/routeList.jsx
+++ src/routes/routeList.jsx
@@ -4,7 +4,7 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import UserDetail from '../pages/UserDetail';
-import ProtectedRoute from './ProtectedRoute';
+import ProtectedRoute from './protectedRoute';
 import Navbar from '../components/Navbar';
 import Landing from '../pages/Landing';
 
